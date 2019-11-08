d6ac6ea9423c6144aa4e958eb6a23432d2b4c35b
Anthony Lopez <anthonybeconscious@gmail.com>
2019-11-08 11:29:45 -0600

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,43 +1,46 @@
-import React, {useState, useEffect} from 'react';
+
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Characters from './components/Characters';
 import './App.css';
-import axios from "axios";
-import Characters from "./components/Characters"
+
 
 const App = () => {
-  const [data, setData] = useState([]);
+
+  const [data, setData]= useState([]);
 
   useEffect(()=>{
-    axios.get('https://lambda-swapi.herokuapp.com/', {})
+    axios
+    .get("https://swapi.co/api/people/", {})
     .then(res=>{
-      console.log(res.data.results)
-      const container = res.data.results;
-      setData(container)
-    })
-    .catch(err=>{
-      console.log('er-er-erra eraa')
-    })
-  },[]);
+      const contain = res.data.results;
+      console.log('results', contain);
+      setData(contain);
+    });
 
+    
+  },[]);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-
-      {data.map(char=>{
+      {data.map(character=>{
         return(
-          <Characters         
-          name = {char.name}
-          gender = {char.gender}
-          height = {char.height}
-          hair_color = {char.hair_color}
-          eye_color = {char.eye_color}
+          <Characters
+          name={character.name}  
+          gender={character.gender}
+          height={character.height}
+          hair_color={character.hair_color}
+          eye_color={character.eye_color}
 
           />
         )
       })}
-
-
     </div>
+  
+
+   
   );
 }
 
