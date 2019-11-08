17c68b6a0c02f137dca0298e0b5fbc4cd29937ea
Anthony Lopez <anthonybeconscious@gmail.com>
2019-11-08 11:14:05 -0600

    tyring to get it to work

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,24 +1,40 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+import Characters from "./components/Characters"
 
 const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(()=>{
-    axios.get('https://swapi.co/api/people/')
+    axios.get('https://lambda-swapi.herokuapp.com/', {})
     .then(res=>{
       console.log(res.data.results)
+      const container = res.data.results;
+      setData(container)
     })
     .catch(err=>{
       console.log('er-er-erra eraa')
     })
-  })
+  },[]);
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+
+      {data.map(char=>{
+        return(
+          <Characters         
+          name = {char.name}
+          gender = {char.gender}
+          height = {char.height}
+          hair_color = {char.hair_color}
+          eye_color = {char.eye_color}
+
+          />
+        )
+      })}
 
 
     </div>
